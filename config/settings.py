15303ea2b180dfe8e12328df2721984a89f0f27b
Polyana Albanaz <polyana.albanaz.ifc@gmail.com>
2024-10-08 09:28:00 -0300

    tutorial 6 finalizado

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vd85x%-%i(e9-hz%6-jw3&y1%3ix42a2w1d5a$p#sc-1h-1e_k'
+SECRET_KEY = 'django-insecure-x@u9ez5cp9x6(_&0$3zgutvff2=0_*vtbpkx1wyea$%2qxdnv9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# config/settings.py
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
